ed4d58e2e0b0269f04704dd1994fe41eb05a62c1
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2024-10-16 09:57:10 +0300

    First commit of the site

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const colorButton = document.getElementById('colorButton');
+
+colorButton.addEventListener('click',()=>{
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor === 'blue' ? '#4caf50' : 'blue';
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+## Hello guys this is my first step in learing Django for website development
